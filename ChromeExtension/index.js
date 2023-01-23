77065740b537e8dd25df384307a2d54ef0731b4d
Alexslzr <117617379+Alexslzr@users.noreply.github.com>
2023-01-23 11:41:02 -0700

    ChromeProject

---
+++ ChromeExtension/index.js
@@ -0,0 +1,24 @@
+
+let myLeads = [];
+let inputBtn = document.getElementById('input-btn');
+let inputEl= document.getElementById('input-el')
+let ulEl= document.getElementById('ul-el');
+
+inputBtn.addEventListener("click", function(){
+   
+    myLeads.push(inputEl.value)
+    inputEl.value='';
+    renderLeads();
+})
+
+function renderLeads() {
+    let listItems = '';
+    for(let i =0; i< myLeads.length;i++){
+        listItems+=`<li>
+                        <a href='${myLeads[i]}' target=_blank>
+                            ${myLeads[i]}
+                        </a>
+                    </li>`
+    }
+    ulEl.innerHTML = listItems;
+}
